f6754fbf579774ebdd4c7e6c352589f7d0231c09
J3MeTron <alfa.mi@hotmail.com>
2022-10-21 09:50:05 +0300

    Added view transformation matrices and object duplication

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <glad/glad.h>  //add glad library
 #include <GLFW/glfw3.h> //add glfw library
 #include <glm/vec2.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 #include <iostream>
 
@@ -9,9 +11,9 @@
 #include "Renderer/Texture2D.h"
 
 GLfloat point[] = {     //массив точек
-     0.0f,  0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-    -0.5f, -0.5f, 0.0f
+     0.0f,  50.f, 0.0f,
+     100.f, -100.f, 0.0f,
+    -100.f, -100.f, 0.0f
 
 };
 
@@ -130,6 +132,16 @@ int main(int argc, char** const argv)
         pDefaultShaderProgram->use();
         pDefaultShaderProgram->setInt("tex", 0);
 
+        glm::mat4 modelMatrix_1 = glm::mat4(1.f);
+        modelMatrix_1 = glm::translate(modelMatrix_1, glm::vec3(100.f, 100.f, 0.f));
+
+        glm::mat4 modelMatrix_2 = glm::mat4(1.f);
+        modelMatrix_2 = glm::translate(modelMatrix_2, glm::vec3(490.f, 100.f, 0.f));
+
+        glm::mat4 projectionMatrix = glm::ortho(0.f, static_cast<float>(g_windowSize.x), 0.f, static_cast<float>(g_windowSize.y), -100.f, 100.f);
+
+        pDefaultShaderProgram->setMatrix4("projectionMat", projectionMatrix);
+
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(pWindow))
         {
@@ -140,6 +152,11 @@ int main(int argc, char** const argv)
             pDefaultShaderProgram->use();
             glBindVertexArray(vao);
             tex->bind();
+
+            pDefaultShaderProgram->setMatrix4("modelMat", modelMatrix_1);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+
+            pDefaultShaderProgram->setMatrix4("modelMat", modelMatrix_2);
             glDrawArrays(GL_TRIANGLES, 0, 3);
 
             /* Swap front and back buffers */
