04212d715276047acec30e8158e00ee8af43f17f
J3MeTron <alfa.mi@hotmail.com>
2022-10-23 10:52:26 +0300

    Added base for working with sprites

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include "Renderer/ShaderProgram.h"
 #include "Resources/ResourceManager.h"
 #include "Renderer/Texture2D.h"
+#include "Renderer/Sprite.h"
 
 GLfloat point[] = {     //массив точек
      0.0f,  50.f, 0.0f,
@@ -94,7 +95,17 @@ int main(int argc, char** const argv)
             return -1;
         }
 
+        auto pSpriteShaderProgram = resourceManager.loadShaders("SpriteShader", "res/shaders/vSprite.txt", "res/shaders/fSprite.txt");
+        if (!pSpriteShaderProgram)
+        {
+            std::cerr << "Can't create shader program: " << "SpriteShader" << std::endl;
+            return -1;
+        }
+
         auto tex = resourceManager.loadTexture("DefaultTexture", "res/textures/map_16x16.png");
+
+        auto pSprite = resourceManager.loadSprite("NewSprite", "DefaultTexture", "SpriteShader", 50, 100);
+        pSprite->setPosition(glm::vec2(300, 100));
 
         /*Передача данных в видеокарту (цвета и точки)                        (&name - ссылка на переменную)*/
         GLuint point_vbo = 0;
@@ -142,6 +153,10 @@ int main(int argc, char** const argv)
 
         pDefaultShaderProgram->setMatrix4("projectionMat", projectionMatrix);
 
+        pSpriteShaderProgram->use();
+        pSpriteShaderProgram->setInt("tex", 0);
+        pSpriteShaderProgram->setMatrix4("projectionMat", projectionMatrix);
+
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(pWindow))
         {
@@ -158,6 +173,8 @@ int main(int argc, char** const argv)
 
             pDefaultShaderProgram->setMatrix4("modelMat", modelMatrix_2);
             glDrawArrays(GL_TRIANGLES, 0, 3);
+
+            pSprite->render();
 
             /* Swap front and back buffers */
             glfwSwapBuffers(pWindow);
